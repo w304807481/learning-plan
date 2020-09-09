f53a83b2b6aab1f894353ebef076f221027fc3a3
jwen <304807481@qq.com>
2020-09-09 23:31:03 +0800

    Springboot 整合servlet

---
+++ framework/src/main/java/com/github/opensharing/framework/springboot/servlet/App.java
@@ -0,0 +1,47 @@
+package com.github.opensharing.framework.springboot.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Spingboot
+ * <p>
+ * 1.增加依赖
+ * <parent>
+ * <groupId>org.springframework.boot</groupId>
+ * <artifactId>spring-boot-starter-parent</artifactId>
+ * <version>2.3.3.RELEASE</version>
+ * <relativePath/> <!-- lookup parent from repository -->
+ * </parent>
+ * <p>
+ * 2.启动类加关键注解
+ *
+ * @author wenzongwei
+ * Date 2020-09-09
+ * @ServletComponentScan 3.编写Servlet, 继承HttpServlet
+ * 3.1 通过注解@WebServlet 配置
+ * 3.2 通过@Bean ServletRegistrationBean 配置
+ */
+@SpringBootApplication
+@ServletComponentScan
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    /**
+     * 配置方式二
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServletRegistrationBean() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new SecondServlet());
+        bean.addUrlMappings("/second");
+        return bean;
+    }
+}
---
+++ framework/src/main/java/com/github/opensharing/framework/springboot/servlet/SecondServlet.java
@@ -0,0 +1,22 @@
+package com.github.opensharing.framework.springboot.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * SecondServlet
+ *
+ * @author wenzongwei
+ * Date 2020-09-09
+ */
+public class SecondServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("SecondServlet....");
+    }
+}
