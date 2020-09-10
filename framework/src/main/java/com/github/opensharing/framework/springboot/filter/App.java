f8ffd679eb45e16c0e315abef6c1cad9adc48731
jwen <304807481@qq.com>
2020-09-10 20:50:05 +0800

    Springboot 整合filter

--- framework/src/main/java/com/github/opensharing/framework/springboot/filter/App.java
+++ framework/src/main/java/com/github/opensharing/framework/springboot/filter/App.java
@@ -10,18 +10,18 @@ import org.springframework.context.annotation.Bean;
  * Spingboot 整合 filter
  * <p>
  * 1.增加依赖
- * <parent>
- * <groupId>org.springframework.boot</groupId>
- * <artifactId>spring-boot-starter-parent</artifactId>
- * <version>2.3.3.RELEASE</version>
- * <relativePath/> <!-- lookup parent from repository -->
- * </parent>
+ *   <parent>
+ *     <groupId>org.springframework.boot</groupId>
+ *     <artifactId>spring-boot-starter-parent</artifactId>
+ *     <version>2.3.3.RELEASE</version>
+ *     <relativePath/> <!-- lookup parent from repository -->
+ *   </parent>
  * <p>
  * 2.启动类加关键注解@SpringBootApplication
  * <p>
  * 3.编写Servlet, 继承HttpServlet
- * 3.1 通过注解@WebFilter 配置
- * 3.2 通过@Bean ServletRegistrationBean 配置
+ *   3.1 通过注解@WebFilter 配置
+ *   3.2 通过@Bean ServletRegistrationBean 配置
  */
 @SpringBootApplication
 @ServletComponentScan
