5521298ef63cfc24067fbacf16f57799c1c76763
jwen <304807481@qq.com>
2022-03-20 20:48:45 +0800

    spring cloud eureka

---
+++ framework/springcloud/registry/eureka/src/main/java/com/github/opensharing/framework/springcloud/registry/eureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.github.opensharing.framework.springcloud.registry.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka server
+ *
+ * @author jwen
+ * Date 2022-03-02
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
