fd5f33f95deefdf1f828eadadff8cb40e398f758
jwen <304807481@qq.com>
2025-08-02 21:59:45 +0800

    补充lambda的案例

---
+++ language-base/javabase/src/main/java/com/github/opensharing/javabase/lambda/ArrayBuilder.java
@@ -0,0 +1,6 @@
+package com.github.opensharing.javabase.lambda;
+
+public interface ArrayBuilder {
+
+    int[] build(int length);
+}
---
+++ language-base/javabase/src/main/java/com/github/opensharing/javabase/lambda/CalculateOperator.java
@@ -0,0 +1,6 @@
+package com.github.opensharing.javabase.lambda;
+
+@FunctionalInterface
+public interface CalculateOperator {
+    int calc(int a, int b);
+}
